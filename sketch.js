2be6bd19cbc8e7e2e4596e44f774bdaeb29c7f53
Ang Jun Ray <angjunray@live.com>
2021-03-13 00:01:56 +0800

    Final Changes! :100: Yay!

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var shade1 = 20, shade2 = 20, shade3 = 50, ran1 = Math.floor(Math.random() * 500) + 1, ran2 = Math.floor(Math.random() * 250) + 250;
+var shade1 = 20, shade2 = 20, shade3 = 50; //shades for the colour rgb
+let ran1 = Math.floor(Math.random() * 500) + 1, ran2 = Math.floor(Math.random() * 250) + 250;
 
 function setup() {
   createCanvas(500, 500);
@@ -21,34 +22,27 @@ function FillRect(width1, height1, width2, height2, fill1, roundedCorner) {
 
 function drawSky() {
   noStroke();
+  //draws the dark blue to light blue gradient
   for (i = 0; i < 100; i++) {
     FillRect(width / 2, height / 2, width - 2 * i, height - 2 * i, color(shade1, shade2, shade3 + i * 2), 120);
   }
   shade3 = 250;
+  //transitions the light blue to orange
   for (i = 0; i < 50; i++) {
     FillRect(width / 2, height / 2, width - 2 * i - 200, height - 2 * i - 200, color(shade1 + (148 / 50) * i, shade2 + (109 / 50) * i, shade3 - i * 4), 120);
   }
   shade1 = 168;
   shade2 = 129;
   shade3 = 50;
+  //transitions the orange to bright yellow
   for (i = 0; i < 50; i++) {
     FillRect(width / 2, height / 2, width - 2 * i - 300, height - 2 * i - 300, color(shade1, shade2 + i * 2, shade3), 120);
   }
   stroke(1);
 }
-/*
-function drawLetter(letter, x, y,  stretch) {
-  push();
-  fill(255);
-  translate(width / 2, height - 20);
-  textAlign(CENTER);
-  textSize(10);
-  scale(stretch, 10);
-  text(letter, x, y);
-  pop();
-}
-*/
+
 function pebble() {
+  //creates 10000 dots that are meant to represent pebbles
   for (i = 0; i < 10000; i++) {
     strokeWeight(50);
     fill("black");
@@ -59,18 +53,12 @@ function pebble() {
 }
 
 function drawRoad() {
+  //drawing the road to the sun
   noStroke();
   push();
   FillRect(width / 2, height * 3 / 4, width, height / 2 + 1, color("grey"), 10);
   filter(BLUR, 5);
   pop();
-  //drawLetter("H", width / 2, height - 20, 40);
-  /*
-  textSize(100)
-  translate(width/2, height-20);
-  rotate(HALF_PI);
-  text("2025", 0, 0);
-  */
   fill("white");
   //pebble the road
   pebble();
@@ -85,10 +73,17 @@ function drawRoad() {
   strokeWeight(7);
   line(40, height, width / 2 - 40, height / 2);
   line(width - 40, height, width / 2 + 40, height / 2);
+  line(width/2 - 40, height/2, width / 2 + 40, height / 2);
   fill("white");
-  stroke("black")
-  textSize(40)
-  //HORIZON and 2025
+  stroke("black");
+  textSize(40);
+
+  //HORIZON AND 2025
+  horizion2025();
+}
+
+function horizion2025() {
+  //creates the horizon and 2025 texts
   text("N", width / 2, height - 10);
   text("O", width / 2, height - 45);
   text("Z", width / 2, height - 80);
@@ -103,10 +98,30 @@ function drawRoad() {
 }
 
 function drawBorder() {
-  fill(0, 0, 0, 10);
-  strokeWeight(10);
-  ellipse(width/2, height/2, 200, 200);
+  //draws the logo border
+  noFill();
+  strokeWeight(5);
+  stroke("black");
+  circle(width/2, height/2, width)
+}
 
+function Manipulate() {
+    //pixel manipulation for crop
+  loadPixels();
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      // assuming height == width
+      if (dist(i, j, width / 2, height / 2) > width / 2) {
+        const idx = (i + j * width) * 4;
+        pixels[idx + 0] = 0;
+        pixels[idx + 1] = 0;
+        pixels[idx + 2] = 0;
+        pixels[idx + 3] = 0;
+
+      }
+    }
+  }
+  updatePixels();
 }
 
 /*
@@ -122,26 +137,7 @@ function draw() {
   drawRoad();
   noStroke();
   fill("white")
-  //pixel manipulation for crop
-  loadPixels();
-  textFont("Fraunces")
-  for (let i = 0; i < width; i++) {
-    for (let j = 0; j < height; j++) {
-      // assuming height == width
-      if (dist(i, j, width / 2, height / 2) > width / 2) {
-        const idx = (i + j * width) * 4;
-        pixels[idx + 0] = 0;
-        pixels[idx + 1] = 0;
-        pixels[idx + 2] = 0;
-        pixels[idx + 3] = 0;
-
-      }
-    }
-  }
-  updatePixels();
+  Manipulate();
   drawBorder();
-  noFill();
-  strokeWeight(5);
-  stroke("black");
-  circle(width/2, height/2, width, height)
+
 }
